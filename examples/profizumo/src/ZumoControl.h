12ef7e7129f4ffb9c96e8e00a3a5fd2171828a27
Moritz Lang <moritz.lang@technikum-wien.at>
2023-03-05 19:22:41 +0100

    Missed in last commit

--- examples/profizumo/src/ZumoControl.h
+++ examples/profizumo/src/ZumoControl.h
@@ -25,6 +25,19 @@ private:
     profinet::Profinet profinet;
     int16_t speedLeft;
     int16_t speedRight;
+    // Inputs
+    //Acceleration
+    int16_t accelerationX{0};
+    int16_t accelerationY{0};
+    int16_t accelerationZ{0};
+    // Gyro 
+    int16_t gyroX{0};
+    int16_t gyroY{0};
+    int16_t gyroZ{0};
+    // Magnetometer 
+    int16_t magnetometerX{0};
+    int16_t magnetometerY{0};
+    int16_t magnetometerZ{0};
     std::unique_ptr<profinet::ProfinetControl> profinetInstance;
 
     bool SendSerial(SerialConnection& serialConnection);
