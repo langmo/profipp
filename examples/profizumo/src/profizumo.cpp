fd2ef8b7fc3f10b6a3d1cb7e8cc14e5ad7515dde
Moritz Lang <moritz.lang@technikum-wien.at>
2023-05-02 10:30:50 +0200

    Possibility to choose network interface and UART port.

--- examples/profizumo/src/profizumo.cpp
+++ examples/profizumo/src/profizumo.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
 #include "ZumoControl.h"
+#include <iostream>
+#include <string>
+#include <vector>
 
-std::string get_option(const std::vector<std::string>& args, const std::string_view& option_name) 
+std::string get_option(const std::vector<std::string>& args, const std::string_view& option_name, const std::string_view& default="") 
 {
     for (auto it = args.begin(), end = args.end(); it != end; ++it) 
     {
@@ -11,7 +13,7 @@ std::string get_option(const std::vector<std::string>& args, const std::string_v
                 return *(it + 1);
         }
     }
-    return "";
+    return default;
 }
 bool has_option(const std::vector<std::string>& args, const std::string_view& option_name) 
 {
@@ -30,18 +32,22 @@ int main(int argc, char *argv[])
     const std::vector<std::string> args(argv, argv + argc);
     const bool start = has_option(args, "-s");
     const std::string filename = get_option(args, "-e");
+    const std::string mainNetworkInterface = get_option(args, "-i", "wlan0");
+    const std::string serial = get_option(args, "-u", "/dev/ttyAMA0")
     if (argc < 2) 
     {
         std::cout << "no options provided!\n";
-        std::cout << "usage: " << argv[0] << " [-s] [-e <path>]\n";
+        std::cout << "usage: " << argv[0] << " [-s] [-e <path>] [-i <interface>] [-u <serial>]\n";
         std::cout << "where: -s will actually start the program (otherwise only initialized)\n";
         std::cout << "       -e will export the profinet configuration as GSDML to <path>. <path> must be an existing folder.\n";
+        std::cout << "       -i sets the network interface name. Defaults to wlan0.\n";
+        std::cout << "       -u sets the serial/COM/uart port. Defaults to /dev/ttyAMA0.\n";
         return 0;
     }
 
     // Run program
     ZumoControl control{};
-    bool success = control.InitializeProfinet();
+    bool success = control.InitializeProfinet(mainNetworkInterface);
     if(!success)
     {
         std::cerr << "Could not initialize profinet configuration.\n";
